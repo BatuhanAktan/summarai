509680ca18dec37e6379fef5e4bafe8cb579bc21
BatuhanAktan <batuhanaktan@gmail.com>
2023-05-09 00:17:06 -0400

    Added Feedback

--- routes/summarize.js
+++ routes/summarize.js
@@ -56,7 +56,7 @@ async function summarize(ask){
       let value = await fetch(
           "https://api-inference.huggingface.co/models/Alred/t5-small-finetuned-summarization-cnn-ver3",
       {
-        headers: { Authorization: "Bearer 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" },
+        headers: { Authorization: "" },
         method: "POST",
         body: JSON.stringify(ask),
           }
